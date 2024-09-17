91e19fad4ba0891d6984742f4accae206f12bfdf
Arthur Bartol <arthurbartol@proton.me>
2024-09-17 19:03:12 -0300

    [~] some changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "include/Utils.h"
+
 #include <iostream>
+#include <fstream>
+#include <string>
 
 static uint32_t width = 800;
 static uint32_t height = 600;
 
 void framebufferSizeCallback(GLFWwindow* pWindow, int width, int height);
 void processInput(GLFWwindow* pWindow);
+
+#define NUM_VAOs 1
+
+GLuint vaos[NUM_VAOs];
+GLuint renderingProgram;
+
+void init(GLFWwindow* pWindow);
+
+static float x = 0.0f;
+static float inc = 0.01f;
+
+void display(GLFWwindow* pWindow, double currentTime);
 
 int main()
 {
@@ -39,7 +55,7 @@ int main()
     if (glewStatus != GLEW_OK)
     {
         std::cerr << "Failed to initialize GLEW: " << glewGetErrorString(glewStatus) << std::endl;
-
+        
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
@@ -48,8 +64,9 @@ int main()
     const GLubyte* renderer = glGetString(GL_RENDERER);
     const GLubyte* version = glGetString(GL_VERSION);
 
-    std::cout << "Renderer: " << renderer << '\n';
-    std::cout << "OpenGL version supported: " << version << '\n';
+    std::clog << "Renderer: " << renderer << std::endl;
+    std::clog << "OpenGL version supported: " << version << std::endl;
+    std::clog << std::endl;
 
     // Register a callback function on the window that
     // gets called each time the window is resized.
@@ -57,12 +74,13 @@ int main()
 
     glfwSwapInterval(1);
 
+    init(pWindow);
+
     while (!glfwWindowShouldClose(pWindow))
     {
         processInput(pWindow);
 
-        glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        display(pWindow, glfwGetTime());
 
         glfwSwapBuffers(pWindow);
         glfwPollEvents();
@@ -85,4 +103,30 @@ void processInput(GLFWwindow* pWindow)
     {
         glfwSetWindowShouldClose(pWindow, true);
     }
+}
+
+void init(GLFWwindow* pWindow)
+{
+    renderingProgram = Utils::createShaderProgram("shaders/vertex.glsl", "shaders/fragment.glsl");
+
+    glGenVertexArrays(NUM_VAOs, vaos);
+    glBindVertexArray(vaos[0]);
+}
+
+void display(GLFWwindow* window, double currentTime)
+{
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(renderingProgram);
+
+    x += inc;
+    if (x > 1.0f) inc = -0.01f;
+    if (x < -1.0f) inc = 0.01f;
+
+    GLuint offsetLocation = glGetUniformLocation(renderingProgram, "offset");   // get ptr to "offset"
+    glProgramUniform1f(renderingProgram, offsetLocation, x);                    // send value "x" to "offset"
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
