322b8a776da305bd98fb35bda321252064016724
Arthur Bartol <arthurbartol@proton.me>
2024-09-18 10:42:51 -0300

    [~] fixed the resolution that I previously changed

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,6 +4,6 @@ out vec4 color;
 
 void main()
 {
-    if (gl_FragCoord.x < 295) color = vec4(1.0, 0.0, 0.0, 1.0);
+    if (gl_FragCoord.x < 400) color = vec4(1.0, 0.0, 0.0, 1.0);
     else                      color = vec4(0.0, 0.0, 1.0, 1.0);
 }
